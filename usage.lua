19d88e8616af99cb8aab9ae4eeb0c9d6a1e0e7d7
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-06-08 00:14:35 +0300

    make usage page done

--- usage.lua
+++ usage.lua
@@ -1,3 +1,9 @@
 function print_usage()
-	print("TODO: add help page!")
+	print("Usage: IP1 IP2 [OPTION]")
+	print("Counts IPs within a range")
+	print()
+	print("IP1 and IP2 must be valid IPv4 or IPv6 addresses")
+	print()
+	print("-ipv4			specify if input is an IPv4 IP address")
+	print("-ipv6			specify if input is an IPv6 IP address")
 end
